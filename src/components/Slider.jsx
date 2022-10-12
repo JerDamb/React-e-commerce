5367dccd62907c0d035bfdff4670cb4afec5cc14
JerDamb <72199699+JerDamb@users.noreply.github.com>
2022-10-12 15:39:44 +0200

    slider is functionnal

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useState } from "react";
+import styled from "styled-components";
 import { sliderItems } from "../data";
+// import { mobile } from "../responsive";
+
+const Arrow = styled.div`
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  background-color: #${(props) => props.bg};
+`;
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
@@ -14,30 +32,36 @@ const Slider = () => {
   };
 
   return (
-    <>
-      <div className="sliderContainer">
-        <div className="arrowLeft" onClick={() => handleClick("left")}>
-          <ArrowBackIosNewIcon />
-        </div>
-        <div className="wrapperSlider">
-          {sliderItems.map((item) => (
-            <div className="slideSlider">
-              <div className="imgContainerSlider">
-                <img src={item.img} alt={item.title} />
-              </div>
-              <div className="infoContainerSlider">
-                <h1>{item.title}</h1>
-                <div className="descriptionSlider">{item.desc}</div>
-                <button>SHOW NOW.</button>
-              </div>
+    <div className="container">
+      <Arrow
+        className="arrows"
+        direction="left"
+        onClick={() => handleClick("left")}
+      >
+        <ArrowBackIosNewIcon />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide className="slide" bg={item.bg} key={item.id}>
+            <div className="imgContainer">
+              <img src={item.img} alt="" />
             </div>
-          ))}
-        </div>
-        <div className="arrowRight" onClick={() => handleClick("right")}>
-          <ArrowForwardIosIcon />
-        </div>
-      </div>
-    </>
+            <div className="infoContainer">
+              <div className="title">{item.title}</div>
+              <div className="description">{item.desc}</div>
+              <button>SHOW NOW</button>
+            </div>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow
+        className="arrows"
+        direction="right"
+        onClick={() => handleClick("right")}
+      >
+        <ArrowForwardIosIcon />
+      </Arrow>
+    </div>
   );
 };
 
